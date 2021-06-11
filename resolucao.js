88d36654d85cf4b08bbe7fdfc69ec0418b939fdf
Marcos Antonio de Santana Junior <marcos.santana@estudante.ufscar.br>
2021-06-11 18:54:46 -0300

    Refatoração da função fix_name: mudança do comparador do for e replace em 1 linha

--- resolucao.js
+++ resolucao.js
@@ -2,10 +2,10 @@ var request = require('./broken-database.json'); // requisição da base de dado
 var data = JSON.parse(JSON.stringify(request)); // transformando a requisição JSON em um arquivo objeto JS para manipulação
 fix_name(data);
 
-function fix_name(data) {
-    for (i = 0; i < 10; i++) {
-        aux = data[i].name.substring(); //conve
-        aux = aux.replace(/¢/g, "c").replace(/æ/g, "a").replace(/æ/g,).replace(/ø/g, "o").replace(/ß/g, "b");
+function fix_name(data) { // função para alterar os caracteres especiais
+    for (i = 0; i < data.length; i++) {
+        aux = data[i].name.substring();
+        aux = aux.replace(/¢/g, "c").replace(/æ/g, "a").replace(/æ/g,).replace(/ø/g, "o").replace(/ß/g, "b"); // expressões regulares para trocar todos os caracteres especiais.
         console.log(aux);
     }
 };
