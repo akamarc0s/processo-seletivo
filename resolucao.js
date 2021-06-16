d7beac62bef0bdf30d0e0820e2f5cf32eb228e4b
Marcos Antonio de Santana Junior <marcos.santana@estudante.ufscar.br>
2021-06-16 11:00:43 -0300

    Comentários da função de contagem do estoque

--- resolucao.js
+++ resolucao.js
@@ -1,17 +1,17 @@
 // MARCOS ANTONIO DE SANTANA JUNIOR
 // PROCESSO SELETIVO ROCKY
 
-// Funções
-function read_json() {
+// ======================================= FUNÇÕES =================================================
+function read_json() { // função de leitura do nosso arquivo JSON com defeitos
     var request = require('./broken-database.json'); // requisição da base de dados
     var data = JSON.parse(JSON.stringify(request)); // transformando a requisição JSON em um arquivo objeto JS para manipulação
     return data;
 }
 
-function create_json(data) {
+function create_json(data) { // função de criação do novo arquivo JSON corrigido
     var fs = require('fs'); // criada uma requisição para guardar o arquivo no disco
-    fs.writeFile("./saida.json", JSON.stringify(data, null, 4), function (err) { // criação de um novo arquivo json "saida.json"
-        if (err) throw err;
+    fs.writeFile("./saida.json", JSON.stringify(data, null, 4), function (err) { // criação de um novo arquivo JSON "saida.json"
+        if (err) throw err; // tratamento de excessão caso não seja possível criar o arquivo
     }
     );
 }
@@ -52,28 +52,30 @@ function sort_name_and_id(data) {  // função que irá imprimir em ordem cresce
     }
 }
 
-function count_stock(data) {
-    var count_panelas = 0, count_eletronico = 0, count_eletrodomestico = 0, count_acessorios = 0;
-    for (i = 0; i < data.length; i++) {
-        if (data[i].category === "Panelas") {
-            count_panelas += (data[i].price * data[i].quantity)
+function count_stock(data) { // função de contagem do estoque de cada categoria
+    var count_panelas = 0, count_eletronico = 0, count_eletrodomestico = 0, count_acessorios = 0; // criação das variavéis que vão armazenar a contagem de cada categoria
+    for (i = 0; i < data.length; i++) { // percorre todo o arquivo JSON comparando sua categoria e adicionando seu (valor x quantidade) a cada variavél de contagem 
+        if (data[i].category === "Panelas") { 
+            count_panelas += (data[i].price * data[i].quantity);
         }
         if (data[i].category === "Eletrodomésticos") {
-            count_eletrodomestico += (data[i].price * data[i].quantity)
+            count_eletrodomestico += (data[i].price * data[i].quantity);
         }
         if (data[i].category === "Eletrônicos") {
-            count_eletronico += (data[i].price * data[i].quantity)
+            count_eletronico += (data[i].price * data[i].quantity);
         }
         if (data[i].category === "Acessórios") {
-            count_acessorios += (data[i].price * data[i].quantity)
+            count_acessorios += (data[i].price * data[i].quantity);
         }
     }
-    var count_total = { count_panelas, count_eletrodomestico, count_eletronico, count_acessorios };
-    console.log(count_total);
+    var count_total = { count_panelas, count_eletrodomestico, count_eletronico, count_acessorios }; // vetor com o valor de todas as contagens
     return count_total;
 
 };
-// Comandos
+// ======================================= FUNÇÕES =================================================
+
+
+// ======================================= COMANDOS ================================================
 var data = read_json();
 fix_name(data);
 fix_price(data);
@@ -81,7 +83,5 @@ fix_quantity(data);
 //console.log(data);
 create_json(data);
 sort_name_and_id(data);
-console.log(data)
 count_stock(data);
-
-
+// ======================================= COMANDOS ================================================
